e7311df000acd1c0a6cbd9f22a7ca7662865cb7c
rinkal02 <150303105145@paruluniversity.ac.in>
2017-03-02 23:31:47 +0530

    Create practicalset1.1.py

---
+++ practicalset1.1.py
@@ -0,0 +1,7 @@
+#WAP to read and display the following information. Name, Address, Phone no.
+name=raw_input("enter your name:")
+address=raw_input("enter your address:")
+phoneno=raw_input("enter your phone number:")
+print "name is %s"%name
+print "address is %s"%address
+print "phoneno is %s"%phoneno
